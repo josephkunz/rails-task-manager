229448a536ed7a8f27fae025800ac822669150ae
Joseph Kunz <joseph.kunz@gmx.de>
2019-11-12 16:37:37 +0100

    dhdhdh

--- config/routes.rb
+++ config/routes.rb
@@ -11,5 +11,7 @@ Rails.application.routes.draw do
    get "tasks/:id/edit", to: "tasks#edit"
    patch "tasks/:id", to: "tasks#update"
 
+  delete "tasks/:id", to: "tasks#destroy"
+
   get "tasks/:id", to: "tasks#show", as: :task
 end
